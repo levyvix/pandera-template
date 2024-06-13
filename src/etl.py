2d147fd89127bfd569d6477847f98734b96d7255
lealre <renaanvp@gmail.com>
2024-06-13 15:02:28 +0100

    Pandera decorator adicionado

--- src/etl.py
+++ src/etl.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import pandera as pa
+from datetime import datetime
 
-from contrato import MetricasFinanceiras
+from contrato import MetricasFinanceirasBase, MetricasFinanceirasOut
 
-# @pa.check_output(MetricasFinanceiras)
+@pa.check_output(MetricasFinanceirasBase)
 def extrai_dados(dir_arquivo: str) -> pd.DataFrame:
     """Extrai os dados, transformando em um dataframe"""
     df = pd.read_csv(dir_arquivo)
     return df
 
+
+@pa.check_output(MetricasFinanceirasOut)
 def transforma_dados(df: pd.DataFrame) -> pd.DataFrame:
     """"Calcula o custo total, faturamento líquido e a margem operacional"""
     df_transformado = df.copy()
     df_transformado["valor_do_imposto"] = df_transformado["percentual_de_imposto"] * df_transformado["receita_operacional"]
     df_transformado["custo_total"] = df_transformado["valor_do_imposto"] + df_transformado["custo_dos_bens"]
     df_transformado["receita_liquida"] = df_transformado["receita_operacional"] - df_transformado["custo_total"]
-    df_transformado["percentual_de_margem_de_lucro"] = (df_transformado["receita_liquida"] / df_transformado["receita_operacional"]) 
+    df_transformado["percentual_de_margem_operacional"] = (df_transformado["receita_liquida"] / df_transformado["receita_operacional"]) 
+    df_transformado["transformado_em"] = datetime.now().strftime('%Y-%m-%d')
     return df_transformado
 
 def carrega_dados(df: pd.DataFrame) -> None:
@@ -26,6 +30,6 @@ def carrega_dados(df: pd.DataFrame) -> None:
 
 if __name__ == '__main__':
     dir_arquivo = 'data/dados_financeiros.csv'
-
     df = extrai_dados(dir_arquivo=dir_arquivo)
     df_transformado = transforma_dados(df)
+    print(df_transformado)
