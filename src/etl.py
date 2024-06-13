395686dce4ba835ccd0ef79bb06eaf44e36b10ab
lealre <renaanvp@gmail.com>
2024-06-13 17:40:07 +0100

    fix: python-dotenv adicionado

--- src/etl.py
+++ src/etl.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import pandera as pa
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 
 from contrato import MetricasFinanceirasBase, MetricasFinanceirasOut
@@ -17,10 +19,9 @@ def extrai_dados(dir_arquivo: str) -> pd.DataFrame:
         MetricasFinanceirasBase.validate(df, lazy=True)
         return df
     except pa.errors.SchemaErrors as exc:
+        print("Erro ao validar os dados:")
         print(exc)
     
-
-
 @pa.check_output(MetricasFinanceirasOut, lazy = True)
 def transforma_dados(df: pd.DataFrame) -> pd.DataFrame:
     """"Calcula o custo total, faturamento líquido e a margem operacional"""
@@ -35,8 +36,19 @@ def transforma_dados(df: pd.DataFrame) -> pd.DataFrame:
 
 def carrega_dados(df: pd.DataFrame) -> None:
     """Carrega os dados no Banco de Dados"""
-    database_uri = "postgresql://user-name:postgres@localhost:5432/mydb"
-    engine = create_engine(database_uri)
+
+    load_dotenv(".env")
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+    POSTGRES_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    engine = create_engine(POSTGRES_DATABASE_URL)
+
     nome_da_tabela = "metricas_financeiras" 
     try:
         df.to_sql(nome_da_tabela, engine, if_exists= "replace", index = False)
